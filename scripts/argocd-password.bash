e1734950dde2143d6c9fb215b8181232d7d7b0d8
xnoto <steven@makeitwork.cloud>
2025-12-28 13:06:54 -0700

    feat: removing unnecessary scripts post oauth

--- scripts/argocd-password.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-oc get secret openshift-gitops-cluster -n openshift-gitops -o jsonpath='{.data.admin\.password}' | base64 -d
-echo
--- scripts/argocd-sync.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-argocd app sync gitops-configs
